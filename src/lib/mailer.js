8cef07ad286c67b8612fdaf3a5c2965fbbe06b8e
Naresh Boda <nboda3230@MacBook-Pro-9.local>
2026-04-01 13:13:15 -0500

    Fix mailer - remove resend dependency

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -1,13 +1,14 @@
-const { Resend } = require('resend');
-
 async function sendEmail({ to, subject, html, triggerType }) {
+  const key = process.env.RESEND_API_KEY;
+  if (!key || key.includes('REPLACE')) {
+    console.log('Email skipped - no RESEND_API_KEY configured');
+    return false;
+  }
   try {
-    const resend = new Resend(process.env.RESEND_API_KEY);
-    await resend.emails.send({
-      from: 'PlaybookOS <onboarding@resend.dev>',
-      to,
-      subject,
-      html
+    const res = await fetch('https://api.resend.com/emails', {
+      method: 'POST',
+      headers: { 'Authorization': `Bearer ${key}`, 'Content-Type': 'application/json' },
+      body: JSON.stringify({ from: 'PlaybookOS <onboarding@resend.dev>', to, subject, html })
     });
     console.log('Email sent to:', to);
     return true;
@@ -16,5 +17,4 @@ async function sendEmail({ to, subject, html, triggerType }) {
     return false;
   }
 }
-
 module.exports = { sendEmail };
